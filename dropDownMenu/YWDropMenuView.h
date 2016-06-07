e7a14a60bad44b108643958e76eb56ed14a0e8d0
yuewenyan <930101433@qq.com>
2016-06-07 09:57:51 +0800

    Update YWDropMenuView.h
    对代理数据源方法提供注释

--- dropDownMenu/YWDropMenuView.h
+++ dropDownMenu/YWDropMenuView.h
@@ -11,12 +11,19 @@
 
 @interface YWIndexPath : NSObject
 
-@property (nonatomic, assign) NSInteger column;
-@property (nonatomic, assign) NSInteger row;
+@property (nonatomic, assign) NSInteger column;   ///< 组
+@property (nonatomic, assign) NSInteger row;      ///< 行
 
+/**
+ *  初始化组和行
+ *
+ *  @param column 组
+ *  @param row    行
+ *
+ *  @return   YWIndexPath
+ */
 - (instancetype)initWithColumn:(NSInteger)column row:(NSInteger)row;
-
-+ (instancetype)indexPathWithColumn:(NSInteger)col row:(NSInteger)row;
++ (instancetype)indexPathWithColumn:(NSInteger)col row:(NSInteger)row; // 同上
 
 @end
 
@@ -27,22 +34,42 @@
 @protocol YWDropMenuViewDatasource <NSObject>
 
 @optional
+
+/**
+ *  menu有几组选择
+ */
 - (NSInteger)numberOfColumnsInMenu:(YWDropMenuView *)dropMenu;
 
+/**
+ *  每一组有多少个选项
+ */
 - (NSInteger)menu:(YWDropMenuView *)dropMenu numberOfRowsInColumn:(NSInteger)column;
 
+/**
+ *  每一个组的标题
+ */
 - (NSString *)menu:(YWDropMenuView *)menu titleForColumn:(NSInteger)column;
 
+/**
+ *  在对应的组里每一行的标题
+ */
 - (NSString *)menu:(YWDropMenuView *)menu titleForRowAtIndexPath:(YWIndexPath *)indexPath;
 
+/**
+ *  当前对应每一组的选择的index
+ */
 - (NSInteger)currentSelectedRow:(NSInteger)column;
 
 @end
 
 @class YWDropMenuView;
+
 @protocol YWDropMenuViewDelegate <NSObject>
 
 @optional
+/**
+ *  选中某一组某一行之后的操作
+ */
 - (void)menu:(YWDropMenuView *)menu didSelectRowAtIndexPath:(YWIndexPath *)indexPath;
 
 @end
@@ -55,36 +82,17 @@
 @property (nonatomic, weak) id<YWDropMenuViewDatasource> dataSource;
 
 @property (nonatomic, strong) UIColor *indicatorColor;
-@property (nonatomic, strong) UIColor *textColor;
-@property (nonatomic, strong) UIColor *separatorColor;
+@property (nonatomic, strong) UIColor *textColor;      ///< 字体颜色
+@property (nonatomic, strong) UIColor *separatorColor;   ///< 分割线颜色
 
+/**
+ *  初始化创建
+ *
+ *  @param origin 左上角坐标
+ *  @param height 行高
+ *
+ *  @return menu
+ */
 - (instancetype)initWithOrigin:(CGPoint)origin andHeight:(CGFloat)height;
 
 @end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
